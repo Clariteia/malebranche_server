d66a4f39cbfb87ee3f478bdd7204823e71f827bc
Alberto Amigo Alonso <albamigh@gmail.com>
2021-12-02 16:15:35 +0100

    - StreamReader to File-Like.
    issue #2

--- src/commands/services.py
+++ src/commands/services.py
@@ -1,5 +1,6 @@
 import logging
 import urllib.parse
+from io import BytesIO
 
 from fastavro import reader
 from minos.cqrs import (
@@ -17,8 +18,10 @@ logger = logging.getLogger(__name__)
 class LogCommandService(CommandService):
     @enroute.rest.command("/logs", "POST")
     async def create_log(self, request: RestRequest) -> Response:
-        c = await request.raw_request.content.read()
-        for record in reader(c):
+        c = request.raw_request.content
+        s = BytesIO(await c.read())
+
+        for record in reader(s):
             logger.info(record)
 
         log = await c.json(loads=urllib.parse.parse_qs)
